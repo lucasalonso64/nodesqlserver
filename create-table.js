17ddc2ed2c390090ae5023ce28df4b3d576d60cd
Lucas <alonsosistemas@gmail.com>
2019-05-27 16:10:54 -0300

    codigo nodesql

---
+++ create-table.js
@@ -0,0 +1,24 @@
+const connStr = "Server=BH-DSI-010084L;Database=node;User Id=sa;Password=epc@123;";
+const sql = require("mssql");
+
+sql.connect(connStr)
+   .then(conn => createTable(conn))
+   .catch(err => console.log("erro! " + err));
+
+
+function createTable(conn){
+ 
+    const table = new sql.Table('Clientes');
+    table.create = true;
+    table.columns.add('ID', sql.Int, {nullable: false, primary: true});
+    table.columns.add('Nome', sql.NVarChar(150), {nullable: false});
+    table.columns.add('CPF', sql.NChar(11), {nullable: false});
+    table.rows.add(1, 'teste1', '12345678901');
+    table.rows.add(2, 'teste2', '09876543210');
+    table.rows.add(3, 'teste3', '12312312399');
+
+    const request = new sql.Request()
+    request.bulk(table)
+           .then(result => console.log('funcionou'))
+           .catch(err => console.log('erro no bulk. ' + err));
+}   
---
+++ index.js
@@ -0,0 +1,41 @@
+﻿const express = require('express');
+const app = express();         
+const bodyParser = require('body-parser');
+const port = 3009; //porta padrão
+const sql = require('mssql');
+const connStr = "Server=BH-DSI-010084L;Database=React;User Id=sa;Password=epc@123;";
+
+//fazendo a conexão global
+sql.connect(connStr)
+   .then(conn => GLOBAL.conn = conn)
+   .catch(err => console.log(err));
+
+//configurando o body parser 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());   
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+
+function execSQLQuery(sqlQry, res){
+    GLOBAL.conn.request()
+               .query(sqlQry)
+               .then(result => res.json(result.recordset))
+               .catch(err => res.json(err));
+}
+// rota que lista todos os clientes
+router.get('/clientes', (req, res) =>{
+    execSQLQuery('SELECT * FROM Contatos', res);
+
+ 
+})
+
+router.delete('/clientes/:id', (req, res) =>{
+    execSQLQuery('DELETE Clientes WHERE ID=' + parseInt(req.params.id), res);
+})
